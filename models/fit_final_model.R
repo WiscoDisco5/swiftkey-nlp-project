a3802c6129e614b717deee89abbcf04387a0628a
John Goodwin <jgoodwinwi@gmail.com>
2021-06-06 21:54:55 -0400

    final model development

---
+++ models/fit_final_model.R
@@ -0,0 +1,34 @@
+## Fit final model
+library(sbo)
+# get data
+rm(list = ls()); gc()
+source("process_data.R")
+
+# sample
+set.seed(451)
+train <- rbernoulli(nrow(en_us_text), .75)
+
+train_text <- en_us_text[train,]
+test_text <- en_us_text[!train,]
+
+rm(en_us_text); gc()
+
+# fit
+ngram_model <- sbo_predictor(object = train_text$text,
+                             N = 3,
+                             dict = target ~ 0.75, # cover 75% of training corpus
+                             .preprocess = sbo::preprocess, # Preprocessing transformation 
+                             EOS = ".?!:;",
+                             lambda = 0.4,
+                             L = 3L, # Number of predictions for input
+                             filtered = c("<UNK>")) # Exclude the <UNK> token from predictions
+
+saveRDS(ngram_model, "./models/ngram_model.rds")                             
+
+# check final error
+rm(train_text); gc()
+set.seed(451)
+eval_model <- eval_sbo_predictor(ngram_model, sample_n(test_text,10000)$text)
+mean(eval_model$correct) # .314
+
+
